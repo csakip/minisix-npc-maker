0387bd50cab6c82f4e6957a8714b5bd81cb05818
Csokav <4134708+csakip@users.noreply.github.com>
2024-04-05 18:20:34 +0100

    Tags

--- src/routes/Initiatives.jsx
+++ src/routes/Initiatives.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { ReactSortable } from "react-sortablejs";
-import { Button, Col, Container, ListGroup, Row, InputGroup, Form } from "react-bootstrap";
+import { Button, Col, Container, ListGroup, Row, InputGroup, Form, Badge } from "react-bootstrap";
 import { parseDice, roll } from "../dice";
 
 const sortableOptions = {
@@ -11,16 +11,24 @@ const sortableOptions = {
   group: "shared",
 };
 
+const tags = [
+  { label: "Kábult", defaultLength: 2, effect: "-3", notes: "-1d mindenre ebben és köv körben." },
+  { label: "Sebesült", defaultLength: undefined, effect: "-3", notes: "-1d mindenre." },
+  { label: "-1d", defaultLength: undefined, effect: "-3", notes: "-1d mindenre." },
+  { label: "-2d", defaultLength: undefined, effect: "-6", notes: "-2d mindenre." },
+  { label: "-3d", defaultLength: undefined, effect: "-9", notes: "-3d mindenre." },
+];
+
 const Initiatives = () => {
   const [characters, setCharacters] = useState([]);
-  const [selectedCharacter, setSelectedCharacter] = useState();
+  const [selectedCharacterId, setSelectedCharacterId] = useState();
 
   useEffect(() => {
     setCharacters(
       [
-        { name: "Character 1", id: 1, initiative: 0, roll: "2d+2" },
-        { name: "Character 2", id: 2, initiative: 0, roll: "1d+2" },
-        { name: "Character 3", id: 3, initiative: 0, roll: "3d" },
+        { name: "Character 1", id: 1, initiative: 0, roll: "2d+2", tags: [], notes: "Sup" },
+        { name: "Character 2", id: 2, initiative: 0, roll: "1d+2", tags: [], notes: "" },
+        { name: "Character 3", id: 3, initiative: 0, roll: "3d", tags: [], notes: "" },
       ].map((c) => {
         c.initiative = roll(parseDice(c.roll));
         return c;
@@ -52,6 +60,28 @@ const Initiatives = () => {
     sortCharacters(characters);
   }
 
+  function toggleTag(selectedCharacterId, tag) {
+    const selectedCharacter = characters.find((c) => c.id === selectedCharacterId);
+    const hadThisTag = selectedCharacter.tags?.find((t) => t.label === tag.label);
+    let newTags;
+    if (!hadThisTag) {
+      newTags = [
+        ...(selectedCharacter.tags ?? []),
+        { label: tag.label, length: tag.defaultLength, effect: tag.effect, notes: tag.notes },
+      ];
+    } else {
+      newTags = selectedCharacter.tags?.filter((t) => t.label !== tag.label) ?? [];
+    }
+
+    setCharacters(
+      characters.map((character) =>
+        character.id === selectedCharacterId ? { ...character, tags: newTags } : character
+      )
+    );
+  }
+
+  const selectedCharacter = characters.find((c) => c.id === selectedCharacterId);
+
   return (
     <Container fluid className='px-3 initiatives'>
       <Row className='pt-2'>
@@ -75,8 +105,8 @@ const Initiatives = () => {
               {characters.map((character) => (
                 <ListGroup.Item
                   key={character.id}
-                  className={selectedCharacter?.id === character.id ? "selected p-0" : "p-0"}>
-                  <Row onClick={() => setSelectedCharacter(character)} className='p-2'>
+                  className={selectedCharacterId === character.id ? "selected p-0" : "p-0"}>
+                  <Row onClick={() => setSelectedCharacterId(character.id)} className='p-2'>
                     <Col xs='1' className='pe-0'>
                       <Button
                         size='sm'
@@ -93,7 +123,14 @@ const Initiatives = () => {
                       </Button>
                     </Col>
                     <Col>
-                      <span className='ps-2'>{character.name}</span>
+                      <span className='ps-2 character-name'>{character.name}</span>
+                    </Col>{" "}
+                    <Col>
+                      {character.tags?.map((tag) => (
+                        <Badge key={tag.label} className='me-1'>
+                          {tag.label} {tag.length}
+                        </Badge>
+                      ))}
                     </Col>
                   </Row>
                 </ListGroup.Item>
@@ -104,7 +141,7 @@ const Initiatives = () => {
         <Col xs='3'>
           <Row>
             <Col className='scrollable-menu'>
-              {selectedCharacter && (
+              {selectedCharacterId && (
                 <>
                   <Row>
                     <Col>
@@ -137,6 +174,38 @@ const Initiatives = () => {
                       {(selectedCharacter.initiative?.rolls?.map((r) => r) || ["-"]).join(", ")}
                     </Col>
                   </Row>
+                  {selectedCharacter.notes && (
+                    <Row>
+                      <Col className='pt-2'>Jegyzetek: {selectedCharacter.notes}</Col>
+                    </Row>
+                  )}
+
+                  <Row>
+                    <Col className='pt-2'>
+                      <div className='d-flex gap-2 flex-wrap'>
+                        {tags?.map((tag) => (
+                          <Button
+                            size='sm'
+                            variant={
+                              selectedCharacter.tags?.some((t) => t.label === tag.label)
+                                ? "primary"
+                                : "outline-secondary"
+                            }
+                            key={tag.label}
+                            onClick={() => toggleTag(selectedCharacterId, tag)}>
+                            {tag.label}
+                          </Button>
+                        ))}
+                      </div>
+                    </Col>
+                  </Row>
+                  {selectedCharacter.tags?.map((tag) => (
+                    <Row key={tag.label}>
+                      <Col className='pt-2'>
+                        {tag.notes} {tag.length && <span>Még {tag.length} kör.</span>}
+                      </Col>
+                    </Row>
+                  ))}
                 </>
               )}
             </Col>
