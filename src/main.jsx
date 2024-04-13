7e59a7b37302bb425d141d2835f2ba2956b75c63
Csokav <4134708+csakip@users.noreply.github.com>
2024-04-13 18:21:04 +0100

    Fix base path with router

--- src/main.jsx
+++ src/main.jsx
@@ -6,23 +6,26 @@ import TopMenu from "./routes/TopMenu";
 import Initiatives from "./routes/Initiatives";
 import Npc from "./routes/Npc";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: (
-      <TopMenu>
-        <Initiatives />
-      </TopMenu>
-    ),
-  },
-  {
-    path: "/npc",
-    element: (
-      <TopMenu>
-        <Npc />
-      </TopMenu>
-    ),
-  },
-]);
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: (
+        <TopMenu>
+          <Initiatives />
+        </TopMenu>
+      ),
+    },
+    {
+      path: "/npc",
+      element: (
+        <TopMenu>
+          <Npc />
+        </TopMenu>
+      ),
+    },
+  ],
+  { basename: "/minisix-npc-maker" }
+);
 
 ReactDOM.createRoot(document.getElementById("root")).render(<RouterProvider router={router} />);
